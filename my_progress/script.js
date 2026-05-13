cb53c231ef02e5f425d9253d63542492ab6c1f99
unknown <kirill.kim.0223@gmail.com>
2026-05-13 15:11:40 +0900

    Начата реализация товарных карточек

---
+++ my_progress/script.js
@@ -0,0 +1,11 @@
+console.log(";)");
+
+
+const serch_button = document.getElementById("serch-button");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.onclick = () => {
+        alert("Пока еще функцианал не реализован")
+    };
+});
