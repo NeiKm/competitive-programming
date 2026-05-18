938aee052da4fb2168bc618f6aa2b8fc4dc8e279
unknown <kirill.kim.0223@gmail.com>
2026-05-18 14:43:51 +0900

    Добавлены карточки товаров

--- my_progress/script.js
+++ my_progress/script.js
@@ -1,11 +1,31 @@
-console.log(";)");
+// console.log(";)");
 
 
-const serch_button = document.getElementById("serch-button");
-const buttons = document.querySelectorAll("button");
+// const serch_button = document.getElementById("serch-button");
+// const buttons = document.querySelectorAll("button");
 
-buttons.forEach((button) => {
-    button.onclick = () => {
-        alert("Пока еще функцианал не реализован")
-    };
-});
+// function md() {
+//     alert(123123);
+// }
+
+// buttons.forEach((button) => {
+//     button.onclick = () => {
+//         alert("Пока еще функцианал не реализован")
+//     };
+// });
+
+// var a = [];
+
+// for (var i = 0; i < 20; i++) {
+//     a[i] = i * 2;
+// }
+
+// for (var i = 0; i < 20; i ++) {
+//     console.log(a[i]);
+// }
+
+var paguni = document.getElementById("paguni");
+function add_product() {
+    console.log(123);
+    paguni.innerHTML("dasd");
+}
